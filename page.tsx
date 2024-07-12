63e4b64b31feaeb4efd9e7b2bc1848ef655777a2
Ramez Festek <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2024-07-11 22:29:35 -0400

    Updated React elements

---
+++ page.tsx
@@ -0,0 +1,41 @@
+// this Typescript file is used to create a more succinct DOM manipulations for the client side 
+
+// messing around with displaying different objects/classes/oop in console
+
+
+// constructor function below:
+// function Person(firstName, lastName, dob) {
+//     this.fistName = firstName;
+//     this.lastName = lastName;
+//     this.dob = new Date(dob);
+//     this.getBirthYear = function() {
+//         return this.dob.getFullYear();
+
+//     }
+// }
+
+
+// const person1 = new Person('hinako', 'sasaki', '8/6/2000');
+// const person2 = new Person('ramez', 'festek', '11/13/1998');
+// const person3 = new Person('chino', 'festek', '12/25/2025');
+
+// console.log(person1.dob.getFullYear);
+
+import React from 'react';
+
+let button0 = 'Button 0';
+let button1 = 'Button 1';
+let button2 = 'Button 2';
+
+
+export default function ButtonId() {
+    return(
+        <button id = (number: 0 )
+
+    )
+
+}
+
+
+
+
